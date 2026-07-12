80d27547aa712f13eb1fcdd56fca60f140948d09
gldn <gldn@github.com>
2026-07-13 01:39:05 +0200

    change nh flake pointer

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -54,7 +54,7 @@ in
                 enable = true;
                 clean.enable = true;
                 clean.extraArgs = "--keep-since 4d --keep 5";
-                flake = system.repo;
+                flake = "home/${user.name}/nixos-config";
             };
             
             nix = {
