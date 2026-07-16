2ce193a66edba583ecec9bbdcc2c26167ccba156
gldn <gldn@github.com>
2026-07-16 15:09:22 +0200

    network secrets

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,34 +1,37 @@
 { lib, ... }:
 let
-    inherit (lib) mkMerge mkIf isAttrs toUpper concatMapStrings stringToCharacters
-    mapAttrs mapAttrsToList listToAttrs nameValuePair flatten concatStringsSep;
+    inherit (lib) mkMerge mkIf;
 in
 {
     flake.modules.nixos.networking = { config, host, ... }:
     let
         inherit (host) hostname user;
-        inherit (config) sops;
         
         # JSON to nix the wifi section of the encrypted secrets.json or nothing if it doesnt exist
-        wifiSecrets = (builtins.fromJSON (builtins.readFile sops.defaultSopsFile)).wifi or { };
+        wifiSecrets = (builtins.fromJSON (builtins.readFile config.sops.defaultSopsFile)).wifi or { };
         
         # Flatten a nested attrset into a list of "/"-joined paths
         # FROM: cafe = { wifi = { ssid=[ENC]; }; wifi-security = { psk=[ENC]; }; };
         # TO: [ "cafe/wifi/ssid" "cafe/wifi-security/psk" ]
-        flattenPaths = prefix: attrs: flatten (
-            mapAttrsToList (name: value:
+        flattenPaths = prefix: attrs: lib.flatten (
+            lib.mapAttrsToList (name: value:
                 let path = "${prefix}/${name}";
-                in if isAttrs value then flattenPaths path value else [ path ]
+                in if lib.isAttrs value then flattenPaths path value else [ path ]
             ) attrs
         );
         
-        wifiPaths = wifiSecrets |> flattenPaths "wifi";
+        wifiPaths = flattenPaths "wifi" wifiSecrets;
         
         # Turn a path string into an env var name
-        # FROM: "wifi/cafe/wifi-security/psk"  TO: "WIFI_CAFE_WIFI_SECURITY_PSK"
-        pathToEnv = string: toUpper (concatMapStrings
-            (char: if builtins.match "[A-Za-z0-9]" char != null then char else "_")
-            (stringToCharacters string));
+        # FROM: "wifi/cafe/wifi-security/psk"
+        # TO: "WIFI_CAFE_WIFI_SECURITY_PSK"
+        pathToEnv = string:
+            toUpper (lib.stringAsChars (c:
+                if builtins.match "[A-Za-z0-9]" c != null
+                then c
+                else "_"
+            ) string);
+
     in
     {
         config = mkMerge [
@@ -41,27 +44,26 @@ in
                 # Initialize secret for each entry
                 sops.secrets =
                     wifiPaths
-                    |> map (p: nameValuePair p { })
-                    |> listToAttrs;
+                    |> map (p: lib.nameValuePair p { })
+                    |> lib.listToAttrs;
                 
                 sops.templates."wifi.env" = {
-                    # FROM: 
-                    # TO: WIFI_CAFE_WIFI_SECURITY_PSK = "${sops.placeholder."wifi/cafe/wifi-security/psk"}"
+                    # WIFI_CAFE_WIFI_SECURITY_PSK = "${sops.placeholder."wifi/cafe/wifi-security/psk"}"
                     content =
                         wifiPaths
-                        |> map (p: "${pathToEnv p}='${sops.placeholder.${p}}'")
-                        |> concatStringsSep "\n";
+                        |> map (p: "${pathToEnv p}='${config.sops.placeholder.${p}}'")
+                        |> lib.concatStringsSep "\n";
                     owner = user.name;
                     restartUnits = [ "NetworkManager-ensure-profiles.service" ];
                 };
                 
                 networking.networkmanager.ensureProfiles = {
-                    environmentFiles = [ sops.templates."wifi.env".path ];
+                    environmentFiles = [ config.sops.templates."wifi.env".path ];
                     
-                    profiles = mapAttrs (name: sections:
+                    profiles = lib.mapAttrs (name: sections:
                         { connection = { id = name; type = "wifi"; }; }
-                        // mapAttrs (section: keys:
-                            mapAttrs (key: _:
+                        // lib.mapAttrs (section: keys:
+                            lib.mapAttrs (key: _:
                                 "$" + pathToEnv "wifi/${name}/${section}/${key}"
                             ) keys
                         ) sections
