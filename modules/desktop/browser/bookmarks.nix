c8d22b820b841b8ccbc1386c0d6535dd991085b1
gldn <gldn@github.com>
2026-08-03 20:18:23 +0200

    bookmark icons

--- modules/desktop/browser/bookmarks.nix
+++ modules/desktop/browser/bookmarks.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 let
-  inherit (lib) concatMap filter;
+  inherit (lib) concatMap filter optionalAttrs;
 
   # Anything tagged "shortcut" also becomes a new-tab tile
   tree = [
@@ -23,6 +23,7 @@ let
         {
           name = "F1TV";
           url = "https://f1tv.formula1.com/";
+          icon = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'><rect width='64' height='64' rx='14' fill='%23e10600'/><path d='M25 19l22 13-22 13z' fill='%23fff'/></svg>";
           tags = [
             "shortcut"
             "streaming"
@@ -31,6 +32,7 @@ let
         {
           name = "F1";
           url = "https://f1live.dpdns.org/1";
+          icon = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'><rect width='64' height='64' rx='14' fill='%2315151e'/><circle cx='32' cy='32' r='5' fill='%23e10600'/><path d='M21 43a15 15 0 0 1 0-22' fill='none' stroke='%23e10600' stroke-width='5' stroke-linecap='round'/><path d='M43 21a15 15 0 0 1 0 22' fill='none' stroke='%23e10600' stroke-width='5' stroke-linecap='round'/></svg>";
           tags = [
             "shortcut"
             "streaming"
@@ -50,10 +52,18 @@ let
 
   flatten = items: concatMap (b: if b ? bookmarks then flatten b.bookmarks else [ b ]) items;
 
-  shortcuts = map (b: {
-    inherit (b) url;
-    label = b.name;
-  }) (filter (b: builtins.elem "shortcut" (b.tags or [ ])) (flatten (tree)));
+  # @TODO: unverified whether the urlbar honours this or only the new tab page.
+  shortcuts = map (
+    b:
+    {
+      inherit (b) url;
+      label = b.name;
+    }
+    // optionalAttrs (b ? icon) {
+      favicon = b.icon;
+      faviconSize = b.iconSize or 96;
+    }
+  ) (filter (b: builtins.elem "shortcut" (b.tags or [ ])) (flatten tree));
 in
 {
   # user-chrome.nix sizes the urlbar from how many tiles there are
