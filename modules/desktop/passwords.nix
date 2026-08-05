75f6646f26d9d6286e8d5ba809bc78f0e77d895a
gldn <gldn@github.com>
2026-08-05 18:24:56 +0200

    try more keepassxc

--- modules/desktop/passwords.nix
+++ modules/desktop/passwords.nix
@@ -9,55 +9,16 @@
           autostart = true;
           enable = true;
           settings = {
-            General = {
-              AutoTypeDelay = 15;
-              AutoTypeStartDelay = 150;
-              BackupBeforeSave = true;
-              GlobalAutoTypeKey = 65;
-              GlobalAutoTypeModifiers = 335544320;
-              URLDoubleClickAction = 1;
-              UpdateCheckMessageShown = true;
-            };
+            Browser.Enabled = true;
+            Browser.BestMatchOnly = true;
+            Browser.SearchInAllDatabases = true;
+            Browser.UnlockDatabase = true;
+            Browser.MatchUrlScheme = true;
             
-            Browser = {
-                Enabled = true;
-                BestMatchOnly = true;
-                SearchInAllDatabases = true;
-            };
+            SSHAgent.Enabled = true;
+            SSHAgent.UseOpenSSH = true;
             
-            GUI = {
-              ApplicationTheme = "auto";
-              ColorPasswords = true;
-              MonospaceNotes = true;
-            };
-            
-            SSHAgent = {
-              Enabled = true;
-              UseOpenSSH = true;
-              AuthSockOverride = "";
-              SecurityKeyProviderOverride = "";
-            };
-            
-            FdoSecrets = {
-              Enabled = true;
-            };
-            
-            PasswordGenerator = {
-                LowerCase = true;
-                UpperCase = true;
-                Numbers = true;
-                
-                AdvancedMode = true;
-                SpecialChars = false;
-                AdditionalChars = ''
-                  !"#$%&()*+,-./:;<=>?@[\]_{|}~
-                '';
-                ExcludedChars = ''
-                  °`´§'^
-                '';
-                
-                Length = 20;
-            };
+            FdoSecrets.Enabled = true;
           };
         };
       };
