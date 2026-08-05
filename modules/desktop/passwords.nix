ca29f34d95dc391995ebfd18bcdf4adab91d0f22
gldn <gldn@github.com>
2026-08-05 19:04:48 +0200

    fixing errors

--- modules/desktop/passwords.nix
+++ modules/desktop/passwords.nix
@@ -8,18 +8,6 @@
         programs.keepassxc = {
           autostart = true;
           enable = true;
-          settings = {
-            Browser.Enabled = true;
-            Browser.BestMatchOnly = true;
-            Browser.SearchInAllDatabases = true;
-            Browser.UnlockDatabase = true;
-            Browser.MatchUrlScheme = true;
-            
-            SSHAgent.Enabled = true;
-            SSHAgent.UseOpenSSH = true;
-            
-            FdoSecrets.Enabled = true;
-          };
         };
       };
     };
