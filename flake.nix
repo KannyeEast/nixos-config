50a98d965c4d24907417ba7bf03b20ac2adc5625
gldn <gldn@github.com>
2026-08-23 18:09:31 +0200

    add wipe commands for impermanence

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
     impermanence = {
       url = "github:nix-community/impermanence";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
 
     ### Home-manager
