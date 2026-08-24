418937c5ba618186aa3ad4819e8971c238317318
gldn <gldn@github.com>
2026-08-24 12:11:52 +0200

    swap postdevicecommands with systemd service

--- modules/system/impermanence.nix
+++ modules/system/impermanence.nix
@@ -1,7 +1,7 @@
-{ lib, inputs, ... }:
+{ inputs, ... }:
 {
   flake.modules.nixos.impermanence =
-    { host, ... }:
+    { pkgs, host, ... }:
     let
       inherit (host) user;
     in
@@ -13,23 +13,33 @@
       config = {
         fileSystems."/persist".neededForBoot = true;
         
-        boot.initrd.postDeviceCommands = lib.mkAfter ''
-          mkdir -p /btrfs_tmp
-          mount -o subvol=/ /dev/disk/by-partlabel/disk-main-root /btrfs_tmp
+        boot.initrd.systemd.initrdBin = [ pkgs.btrfs-progs ];
         
-          delete_subvolume_recursively() {
-            IFS=$'\n'
-            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
-            done
-            btrfs subvolume delete "$1"
-          }
-          
-          delete_subvolume_recursively /btrfs_tmp/root
-          btrfs subvolume create /btrfs_tmp/root
-          
-          umount /btrfs_tmp
-        '';
+        boot.initrd.systemd.services.rollback = {
+          description = "Reset the root subvolume to a blank state";
+          wantedBy = [ "initrd.target" ];
+          after = [ "initrd-root-device.target" ];
+          before = [ "sysroot.mount" ];
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            mkdir -p /btrfs_tmp
+            mount -o subvol=/ /dev/disk/by-partlabel/disk-main-root /btrfs_tmp
+
+            delete_subvolume_recursively() {
+              IFS=$'\n'
+              for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+              done
+              btrfs subvolume delete "$1"
+            }
+
+            delete_subvolume_recursively /btrfs_tmp/root
+            btrfs subvolume create /btrfs_tmp/root
+
+            umount /btrfs_tmp
+          '';
+          };
         
         systemd.tmpfiles.rules = [
           "Z /persist/home/${user.name} - ${user.name} users -"
